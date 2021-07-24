0700f3510c71a349f142cee2cfa488294987280d
dosisod <39638017+dosisod@users.noreply.github.com>
2021-07-24 16:42:53 -0700

    Apply patches:
    * alpha
    * numbers
    * center
    * border
    * fuzzymatch
    * fuzzyhightlight
    * grid
    * gridnav

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ My custom build of `dmenu`.
 
 ## Patches
 
+* [alpha](https://tools.suckless.org/dmenu/patches/alpha/)
+* [numbers](https://tools.suckless.org/dmenu/patches/numbers/)
+* [center](https://tools.suckless.org/dmenu/patches/center/)
+* [border](https://tools.suckless.org/dmenu/patches/border/)
+* [fuzzymatch](https://tools.suckless.org/dmenu/patches/fuzzymatch/)
+* [fuzzyhighlight](https://tools.suckless.org/dmenu/patches/fuzzyhighlight/)
+* [grid](https://tools.suckless.org/dmenu/patches/grid/)
+* [gridnav](https://tools.suckless.org/dmenu/patches/gridnav/)
 * Change Font
 
 Original copy of `README`:
--- config.h
+++ config.h
@@ -2,6 +2,11 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xE0;
+static unsigned border_width = 0;
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrains Mono:size=10",
@@ -12,10 +17,20 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#ffeea5", "#005577" },
+	[SchemeNormHighlight] = { "#fff5cc", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
 
 /*
  * Characters not considered part of a word while deleting words
